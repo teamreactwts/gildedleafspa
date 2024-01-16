d1aa6e68f113df99cb2a319cab07232525e25371
webskitters <webskitters@webskitterss-iMac.local>
2024-01-16 11:58:33 +0530

    pull

---
+++ pages/survice.tsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled'
+import { Box } from '@mui/system'
+import React from 'react'
+
+
+
+export const SurviceWrap=styled(Box)`
+    
+`
+
+function Survice() {
+  return (
+   <SurviceWrap>
+
+   </SurviceWrap>
+  )
+}
+
+export default Survice
