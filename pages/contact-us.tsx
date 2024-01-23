fb3b0c6da3f11264ab661c7b19bb9037630d8429
shammaparveen <mirshamma.parveen@webskitters.in>
2024-01-23 19:49:40 +0530

    issues

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -79,167 +79,170 @@ const Index = () => {
         <BreadcumbTitle title="Contact Us" pageName="Contact Us" />
         <ContactUsWrapper className="cmn_gap">
           <Container fixed>
-            <Grid
-              container
-              spacing={{ lg: 17, md: 10, xs: 3 }}
-              alignItems="center"
-            >
-              <Grid item md={5.5} xs={12}>
-                <Box className="contact_form_content">
-                  <Typography variant="h3">Contact details</Typography>
-                  <Typography>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's{" "}
-                  </Typography>
-                  <List disablePadding>
-                    <ListItem disablePadding>
-                      <Typography component="i">
-                        <MailIcon
-                          IconColor={primaryColors?.colorA67033}
-                          IconWidth="20"
-                          IconHeight="20"
-                        />
-                      </Typography>
-                      <Box className="contact_details">
-                        <Typography variant="h6">Mail</Typography>
-                        <Link
-                          href={`mailto:${settingsDetails?.data?.data?.email}`}
-                        >
-                          {settingsDetails?.data?.data?.email}
-                        </Link>
-                      </Box>
-                    </ListItem>
-                    <ListItem disablePadding>
-                      <Typography component="i">
-                        <PhoneIcon
-                          IconColor={primaryColors?.colorA67033}
-                          IconWidth="20"
-                          IconHeight="20"
-                        />
-                      </Typography>
-                      <Box className="contact_details">
-                        <Typography variant="h6">Call</Typography>
-                        <Link
-                          href={`tel:${settingsDetails?.data?.data?.contactNumber}`}
-                        >
-                          {settingsDetails?.data?.data?.contactNumber}
-                        </Link>
-                      </Box>
-                    </ListItem>
-                    <ListItem disablePadding>
-                      <Typography component="i">
-                        <LocationIcon
-                          IconColor={primaryColors?.colorA67033}
-                          IconWidth="20"
-                          IconHeight="20"
-                        />
-                      </Typography>
-                      <Box className="contact_details">
-                        <Typography variant="h6">Location</Typography>
-                        <Typography component="address">
-                          {settingsDetails?.data?.data?.address}
+            <Box className="contact_form_otr">
+              <Grid
+                container
+                columnSpacing={{ lg: 17, md: 10, xs: 3 }}
+                rowSpacing={{ xs: 3 }}
+                alignItems="center"
+              >
+                <Grid item md={5.5} xs={12}>
+                  <Box className="contact_form_content">
+                    <Typography variant="h3">Contact details</Typography>
+                    <Typography>
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry. Lorem Ipsum has been the industry's{" "}
+                    </Typography>
+                    <List disablePadding>
+                      <ListItem disablePadding>
+                        <Typography component="i">
+                          <MailIcon
+                            IconColor={primaryColors?.colorA67033}
+                            IconWidth="20"
+                            IconHeight="20"
+                          />
                         </Typography>
+                        <Box className="contact_details">
+                          <Typography variant="h6">Mail</Typography>
+                          <Link
+                            href={`mailto:${settingsDetails?.data?.data?.email}`}
+                          >
+                            {settingsDetails?.data?.data?.email}
+                          </Link>
+                        </Box>
+                      </ListItem>
+                      <ListItem disablePadding>
+                        <Typography component="i">
+                          <PhoneIcon
+                            IconColor={primaryColors?.colorA67033}
+                            IconWidth="20"
+                            IconHeight="20"
+                          />
+                        </Typography>
+                        <Box className="contact_details">
+                          <Typography variant="h6">Call</Typography>
+                          <Link
+                            href={`tel:${settingsDetails?.data?.data?.contactNumber}`}
+                          >
+                            {settingsDetails?.data?.data?.contactNumber}
+                          </Link>
+                        </Box>
+                      </ListItem>
+                      <ListItem disablePadding>
+                        <Typography component="i">
+                          <LocationIcon
+                            IconColor={primaryColors?.colorA67033}
+                            IconWidth="20"
+                            IconHeight="20"
+                          />
+                        </Typography>
+                        <Box className="contact_details">
+                          <Typography variant="h6">Location</Typography>
+                          <Typography component="address">
+                            {settingsDetails?.data?.data?.address}
+                          </Typography>
+                        </Box>
+                      </ListItem>
+                    </List>
+                  </Box>
+                </Grid>
+                <Grid item md={6.5} xs={12}>
+                  <Box className="contact_form">
+                    <Typography variant="h3">Get in touch</Typography>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                      <Box className="each_input_field">
+                        <Controller
+                          control={control}
+                          name="full_name"
+                          render={({
+                            field: { onChange, onBlur, value, name, ref }
+                          }) => (
+                            <InputFieldCommon
+                              placeholder="Name"
+                              className="transparent_input"
+                              onChange={onChange}
+                              value={value}
+                              error={Boolean(errors?.full_name)}
+                              helperText={errors?.full_name?.message}
+                            />
+                          )}
+                        />
                       </Box>
-                    </ListItem>
-                  </List>
-                </Box>
+                      <Box className="each_input_field">
+                        <Controller
+                          control={control}
+                          name="email"
+                          render={({
+                            field: { onChange, onBlur, value, name, ref }
+                          }) => (
+                            <InputFieldCommon
+                              placeholder="Email"
+                              className="transparent_input"
+                              onChange={onChange}
+                              value={value}
+                              error={Boolean(errors?.email)}
+                              helperText={errors?.email?.message}
+                            />
+                          )}
+                        />
+                      </Box>
+                      <Box className="each_input_field">
+                        <Controller
+                          control={control}
+                          name="phone"
+                          render={({
+                            field: { onChange, onBlur, value, name, ref }
+                          }) => (
+                            <InputFieldCommon
+                              placeholder="Phone No"
+                              className="transparent_input"
+                              onChange={onChange}
+                              value={value}
+                              error={Boolean(errors?.phone)}
+                              helperText={errors?.phone?.message}
+                            />
+                          )}
+                        />
+                      </Box>
+                      <Box className="each_input_field">
+                        <Controller
+                          control={control}
+                          name="message"
+                          render={({
+                            field: { onChange, onBlur, value, name, ref }
+                            // fieldState:{invalid,error}
+                          }) => (
+                            <InputFieldCommon
+                              placeholder="Message"
+                              multiline
+                              rows={4}
+                              maxRows={4}
+                              className="transparent_input"
+                              onChange={onChange}
+                              value={value}
+                              error={Boolean(errors?.message)}
+                              helperText={errors?.message?.message}
+                            />
+                          )}
+                        />
+                      </Box>
+                      <Box className="each_input_field">
+                        <CustomButtonPrimary
+                          variant="contained"
+                          color="primary"
+                          sx={{ width: "100%" }}
+                          type="submit"
+                          loading={isLoading}
+                          disabled={isLoading}
+                        >
+                          <Typography variant="caption">Submit</Typography>
+                        </CustomButtonPrimary>
+                      </Box>
+                    </form>
+                  </Box>
+                </Grid>
               </Grid>
-              <Grid item md={6.5} xs={12}>
-                <Box className="contact_form">
-                  <Typography variant="h3">Get in touch</Typography>
-                  <form onSubmit={handleSubmit(onSubmit)}>
-                    <Box className="each_input_field">
-                      <Controller
-                        control={control}
-                        name="full_name"
-                        render={({
-                          field: { onChange, onBlur, value, name, ref }
-                        }) => (
-                          <InputFieldCommon
-                            placeholder="Name"
-                            className="transparent_input"
-                            onChange={onChange}
-                            value={value}
-                            error={Boolean(errors?.full_name)}
-                            helperText={errors?.full_name?.message}
-                          />
-                        )}
-                      />
-                    </Box>
-                    <Box className="each_input_field">
-                      <Controller
-                        control={control}
-                        name="email"
-                        render={({
-                          field: { onChange, onBlur, value, name, ref }
-                        }) => (
-                          <InputFieldCommon
-                            placeholder="Email"
-                            className="transparent_input"
-                            onChange={onChange}
-                            value={value}
-                            error={Boolean(errors?.email)}
-                            helperText={errors?.email?.message}
-                          />
-                        )}
-                      />
-                    </Box>
-                    <Box className="each_input_field">
-                      <Controller
-                        control={control}
-                        name="phone"
-                        render={({
-                          field: { onChange, onBlur, value, name, ref }
-                        }) => (
-                          <InputFieldCommon
-                            placeholder="Phone No"
-                            className="transparent_input"
-                            onChange={onChange}
-                            value={value}
-                            error={Boolean(errors?.phone)}
-                            helperText={errors?.phone?.message}
-                          />
-                        )}
-                      />
-                    </Box>
-                    <Box className="each_input_field">
-                      <Controller
-                        control={control}
-                        name="message"
-                        render={({
-                          field: { onChange, onBlur, value, name, ref }
-                          // fieldState:{invalid,error}
-                        }) => (
-                          <InputFieldCommon
-                            placeholder="Message"
-                            multiline
-                            rows={4}
-                            maxRows={4}
-                            className="transparent_input"
-                            onChange={onChange}
-                            value={value}
-                            error={Boolean(errors?.message)}
-                            helperText={errors?.message?.message}
-                          />
-                        )}
-                      />
-                    </Box>
-                    <Box className="each_input_field">
-                      <CustomButtonPrimary
-                        variant="contained"
-                        color="primary"
-                        sx={{ width: "100%" }}
-                        type="submit"
-                        loading={isLoading}
-                        disabled={isLoading}
-                      >
-                        <Typography variant="caption">Submit</Typography>
-                      </CustomButtonPrimary>
-                    </Box>
-                  </form>
-                </Box>
-              </Grid>
-            </Grid>
+            </Box>
           </Container>
         </ContactUsWrapper>
       </InnnerPageWrapper>
